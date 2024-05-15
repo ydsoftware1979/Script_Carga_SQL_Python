27a8388aeb09335411f88fe42cd3a466af312b80
Alexandre Yamamoto <alextoyama@hotmail.com>
2024-05-15 09:04:07 -0300

    atualiza

--- script_pyton.py
+++ script_pyton.py
@@ -9,6 +9,26 @@ faker = Faker()
 # Número de registros fictícios a serem gerados
 num_records = 100
 
+# Função para gerar CNPJ fictício formatado
+def generate_fake_cnpj():
+    """
+    Generates a fake CNPJ with the correct format.
+
+    Returns:
+        str: A string representing a fake CNPJ.
+    """
+    def calculate_cnpj_digit(cnpj):
+        weight = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+        total = sum(int(n) * weight[i] for i, n in enumerate(cnpj))
+        remainder = total % 11
+        return '0' if remainder < 2 else str(11 - remainder)
+    
+    base_cnpj = ''.join([str(random.randint(0, 9)) for _ in range(8)]) + '0001'
+    first_digit = calculate_cnpj_digit(base_cnpj)
+    second_digit = calculate_cnpj_digit(base_cnpj + first_digit)
+    return f"{base_cnpj[:2]}.{base_cnpj[2:5]}.{base_cnpj[5:8]}/{base_cnpj[8:12]}-{first_digit}{second_digit}"
+
+
 # Função para gerar dados fictícios e salvar em um arquivo Excel
 def generate_and_save_fake_data(num_records, filename):
     """
@@ -24,11 +44,15 @@ def generate_and_save_fake_data(num_records, filename):
     data = []
     for _ in range(num_records):
         nf = random.randint(1, 999999)
+        emissao = faker.date_between(start_date='-1y', end_date='today')  # Data de emissão no último ano até hoje
+        emissao_formatada = emissao.strftime('%d/%m/%Y')  # Formata a data para DD/MM/AAAA
+        
+        empresa = faker.company()
         fornecedor_id = random.randint(1, 999999)  # Gere IDs únicos (neste exemplo)
         nome = faker.name()
         emissao = faker.date_between(start_date='-1y', end_date='today')  # Data de emissão no último ano até hoje
         emissao_formatada = emissao.strftime('%d/%m/%Y')  # Formata a data para DD/MM/AAAA
-        data.append({'fornecedor_id': fornecedor_id, 'name': nome, 'emissao': emissao_formatada})
+        data.append({'Nº NF': nf, 'Cliente': empresa, 'name': nome, 'emissao': emissao_formatada})
 
         #data.append({'Nº NF': nf, 'fornecedor_id': fornecedor_id, 'name': nome})
     df = pd.DataFrame(data)
